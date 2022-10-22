57706d0493a74f9afd228bca2c7d51c1c0613775
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-10-22 14:03:08 +0200

    Created first InputUtil method enterInteger

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,7 +1,19 @@
 package main;
 
+import util.InputUtil;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Welcome to the School's Operation Center");
+        int operation = InputUtil.enterInteger("What do you want to do?"+
+                "/n1: Register Student"+
+                "/n2: Show All Students"+
+                "/n3: Find A Student"+
+                "/n4: Update a Student"+
+                "/n5: Quit"
+                );
 
     }
 }
--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -1,4 +1,16 @@
 package util;
 
+import java.util.Scanner;
+
 public class InputUtil {
+    public static int enterInteger(String title){
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        int operation = scanner.nextInt();
+        return operation;
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
