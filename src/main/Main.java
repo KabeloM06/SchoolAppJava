6901469aff9474709bb95e2791b9eb19f83ee81d
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-10-22 14:27:41 +0200

    Created InputUtil method enterString

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,12 +8,16 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the School's Operation Center");
         int operation = InputUtil.enterInteger("What do you want to do?"+
-                "/n1: Register Student"+
-                "/n2: Show All Students"+
-                "/n3: Find A Student"+
-                "/n4: Update a Student"+
-                "/n5: Quit"
+                "\n1: Register Student"+
+                "\n2: Show All Students"+
+                "\n3: Find A Student"+
+                "\n4: Update a Student"+
+                "\n5: Quit"
                 );
+
+        if(operation==1){//register student
+            InputUtil.enterString("How many students are you registering?");
+        }
 
     }
 }
--- src/util/InputUtil.java
+++ src/util/InputUtil.java
@@ -9,6 +9,12 @@ public class InputUtil {
         int operation = scanner.nextInt();
         return operation;
     }
+    public static String enterString(String title){
+        System.out.println(title);
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+        return answer;
+    }
 
     public static void main(String[] args) {
 
